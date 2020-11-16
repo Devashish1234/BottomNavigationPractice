73ee8eff9cb78fc974c70e151e69e585d3c678da
Devashish1234 <32618043+Devashish1234@users.noreply.github.com>
2020-11-16 10:27:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.bottomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment fragment = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.homeBottomNav:
+
+                        fragment = new BlankFragment1();
+                        break;
+
+                    case R.id.cartBottomNav:
+                        fragment = new BlankFragment2();
+                        break;
+
+
+
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+                return false;
+            }
+        });
+    }
+}
